8a0a9d6fd458b75e41fdafd8c69b8e7f44680770
jaso69 <joseonieva@gmail.com>
2024-03-04 22:40:19 +0100

    control m32r

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const auditorioButton = document.querySelector('#auditorio')
+const m32rButton = document.querySelector('#m32r')
 
 const auditorioImg = document.querySelector('#auditorioImg')
+const m32rImg = document.querySelector('#m32rImg')
+
+m32rButton.addEventListener('mouseover', () => {
+    m32rImg.classList.remove('cerrar')
+    m32rImg.classList.add('abrir') 
+})
+
+m32rButton.addEventListener('mouseout', () => {
+    m32rImg.classList.remove('abrir')
+    m32rImg.classList.add('cerrar')  
+})
 
 auditorioButton.addEventListener('mouseover', () => {
     auditorioImg.classList.remove('cerrar')
---
+++ m32r.js
@@ -0,0 +1,29 @@
+const modal = document.querySelector('#dialogo')
+const buttonYes = document.querySelector('#yes')
+const buttonNo = document.querySelector('#no')
+
+const start = () => {
+  introJs().oncomplete(async () => {
+      return new Promise((resolve) => {
+        end()
+        setInterval(resolve, 500);
+      });
+  }).onbeforeexit(function () {
+    return end();
+  }).start()
+} 
+
+buttonYes.addEventListener('click', () => { window.location.replace('./m32rBack.html')})
+
+buttonNo.addEventListener('click', () => {window.location.replace('./index.html')})
+
+document.querySelector('#dialogo').style.display = 'none'
+
+
+const end = () => {
+  document.querySelector('#dialogo').style.display = 'block'
+  modal.showModal()
+}
+
+
+start()
