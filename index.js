d124aa30cc09142930dc8dbef46d44c8f07fb84e
jaso69 <joseonieva@gmail.com>
2024-03-09 00:40:22 +0100

    allen

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const auditorioButton = document.querySelector('#auditorio')
 const m32rButton = document.querySelector('#m32r')
+const sq5Button = document.querySelector('#sq5')
 
 const auditorioImg = document.querySelector('#auditorioImg')
 const m32rImg = document.querySelector('#m32rImg')
+const sq5Img = document.querySelector('#sq5Img')
+
+sq5Button.addEventListener('mouseover', () => {
+    sq5Img.classList.remove('cerrar')
+    sq5Img.classList.add('abrir') 
+})
+
+sq5Button.addEventListener('mouseout', () => {
+    sq5Img.classList.remove('abrir')
+    sq5Img.classList.add('cerrar')  
+})
 
 m32rButton.addEventListener('mouseover', () => {
     m32rImg.classList.remove('cerrar')
