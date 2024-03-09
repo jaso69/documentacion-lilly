56433cd9c60d8bacb1026840c43245b6978279f2
jaso69 <joseonieva@gmail.com>
2024-03-09 20:23:35 +0100

    end sq5

---
+++ sq5.js
@@ -0,0 +1,32 @@
+const modal = document.querySelector('#dialogo')
+const buttonYes = document.querySelector('#yes')
+const buttonNo = document.querySelector('#no')
+
+const start = () => {
+  introJs().oncomplete(async () => {
+      return new Promise((resolve) => {
+        next()
+        setInterval(resolve, 500);
+      });
+  }).onbeforeexit(function () {
+    return end();
+  }).start()
+} 
+
+buttonYes.addEventListener('click', () => { window.location.replace('./sq5.html')})
+
+buttonNo.addEventListener('click', () => {window.location.replace('./index.html')})
+
+document.querySelector('#dialogo').style.display = 'none'
+
+const next = () => {
+  window.location.replace('./m32rControl.html')
+}
+
+const end = () => {
+  document.querySelector('#dialogo').style.display = 'block'
+  modal.showModal()
+}
+
+
+start()
