447534a4c6e6acd5af67ae87202edc6c9662ecd8
jaso69 <joseonieva@gmail.com>
2024-03-13 17:14:00 +0100

    data

--- asis.js
+++ asis.js
@@ -2,14 +2,14 @@
 const label = document.querySelector('#resp')
 const pregunta = document.querySelector('#pregunta')
 const boton = document.querySelector('#boton')
-const url = 'https://openai-rpg.vercel.app/gpt/check'
 let prompt = {}
-
+let url;
 boton.addEventListener('click', () => {
     const p = String(pregunta.value)
     pregunta.value = ''
     if(p.length < 1) return
     prompt = {prompt: p}
+    url = 'http://194.164.166.107/api/gpt?prompt' + p
     Nuevo(prompt)
 })
 
@@ -21,7 +21,8 @@ const respuesta = (resp) => {
 async function Nuevo (prompt){
     const data = await fetch( url, 
         {   method: 'POST',
-            headers: {'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json'}, 
+            mode: 'cors',
+            headers: {'Content-Type': 'application/json'}, 
             body: JSON.stringify( prompt ) 
         })
     const resp = await data.json()
