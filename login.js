e53f0e4ea045d6b91d8cab495a320afcf04cedb4
jaso69 <joseonieva@gmail.com>
2024-03-16 22:51:33 +0100

    add error css

--- login.js
+++ login.js
@@ -1,6 +1,7 @@
 const userText = 'Lilly'
 const passText = '123456'
 
+const error = document.querySelector('#borde')
 const user = document.querySelector('#user')
 const pass = document.querySelector('#pass')
 const submit = document.querySelector('#submit')
@@ -9,5 +10,9 @@ submit.addEventListener('click', () => {
     if (userText === user.value && passText === pass.value) {
         window.localStorage.setItem('login', 'ok')
         window.location.replace('./index.html')
+    } else {
+        error.classList.add('error')
+        user.value = ''
+        pass.value = ''
     }
 })
